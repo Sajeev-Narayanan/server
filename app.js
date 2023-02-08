28a7723b9b2df783206a8d516d24de5875037f42
sajeev-narayanan <sajeevnarayanan817@gmail.com>
2023-02-08 11:12:04 +0530

    cors problem

--- app.js
+++ app.js
@@ -39,15 +39,15 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Contol-Allow-Orgin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Contol-Allow-Orgin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   next();
+// });
 
 
 app.use("/", userRoutes);
--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,21 +1,21 @@
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-  }
-  
+  require("dotenv").config();
+}
+
 const mongoose = require("mongoose");
-  
+
 mongoose.set('strictQuery', false);
 const dbconfig = async () => {
-    try {
-      await mongoose.connect(process.env.mongoConnect, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log("CONNECTED 👍");
-    } catch {
-      console.log("OH NO MONGO CONNECTION ERROR!!!!");
-      // console.log(err);
-      process.exit();
-    }
-  };
+  try {
+    await mongoose.connect(process.env.mongoConnect, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("CONNECTED 👍");
+  } catch (error) {
+    console.log("OH NO MONGO CONNECTION ERROR!!!!");
+    console.log(error);
+    process.exit();
+  }
+};
 module.exports = dbconfig;
